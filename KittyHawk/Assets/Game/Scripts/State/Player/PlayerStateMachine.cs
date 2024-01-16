98d9db8feacb995e81876ffcdf9ea521a9fe185a
Geoffrey Roth <geoffroth@gmail.com>
2024-01-16 10:34:28 -0500

    Added downwarn gravity force to player

--- KittyHawk/Assets/Game/Scripts/State/Player/PlayerStateMachine.cs
+++ KittyHawk/Assets/Game/Scripts/State/Player/PlayerStateMachine.cs
@@ -7,6 +7,7 @@ public class PlayerStateMachine : StateMachine
     [field: SerializeField] public CharacterController Controller { get; private set; }
     [field: SerializeField] public InputReader InputReader { get; private set; }
     [field: SerializeField] public Animator Animator { get; private set; }
+    [field: SerializeField] public ForceReceiver ForceReceiver { get; private set; }
     [field: SerializeField] public float FreeMovementSpeed { get; private set; }
     [field: SerializeField] public float RotationDamping { get; private set; }
 
@@ -16,5 +17,10 @@ public class PlayerStateMachine : StateMachine
     {
         MainCameraTransform = Camera.main.transform;
         SwitchState(new PlayerFreeLookState(this));
+    }
+
+    protected void Move(Vector3 movement, float deltaTime)
+    {
+
     }
 }
