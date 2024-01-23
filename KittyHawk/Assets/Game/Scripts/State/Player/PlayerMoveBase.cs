857b904068b0a0e5b8c36aeea6d0b58a997e0ad1
Geoffrey Roth <geoffroth@gmail.com>
2024-01-22 21:11:35 -0500

    Feature/jumping (#53)
    * Basic jumping
    
    * Add mid-air movement control to jump sequence
    
    * Add running
    
    * Continue running after jumping

---
+++ KittyHawk/Assets/Game/Scripts/State/Player/PlayerMoveBase.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMoveBase : PlayerBaseState
+{
+    public PlayerMoveBase(PlayerStateMachine stateMachine, bool isRunning = false) : base(stateMachine)
+    {
+        this.isRunning = isRunning;
+    }
+    public override void Enter()
+    {
+        stateMachine.InputReader.RunEvent += OnRun;
+        stateMachine.InputReader.RunStopEvent += OnRunStop;
+    }
+
+    public override void Tick(float deltaTime) {}
+
+    public override void Exit()
+    {
+        stateMachine.InputReader.RunEvent -= OnRun;
+        stateMachine.InputReader.RunStopEvent -= OnRunStop;
+    }
+
+    protected bool isRunning = false;
+
+    protected Vector3 CalculateMovement()
+    {
+        Vector3 forward = stateMachine.MainCameraTransform.forward;
+        Vector3 right = stateMachine.MainCameraTransform.right;
+        forward.y = 0f;
+        right.y = 0f;
+        forward.Normalize();
+        right.Normalize();
+        return forward * stateMachine.InputReader.MovementValue.y +
+            right * stateMachine.InputReader.MovementValue.x;
+    }
+
+    protected void FaceMovementDirection(Vector3 movement, float deltaTime)
+    {
+        stateMachine.transform.rotation = Quaternion.Lerp(
+            stateMachine.transform.rotation,
+            Quaternion.LookRotation(movement),
+            deltaTime * stateMachine.RotationDamping
+        );
+    }
+
+    protected void OnRun()
+    {
+        isRunning = true;
+    }
+
+    protected void OnRunStop()
+    {
+        isRunning = false;
+    }
+}
