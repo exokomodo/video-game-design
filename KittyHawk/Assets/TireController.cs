2c43286d202d5bd2faf9a6bba619d92417d0da0c
Paul Garza <paulvgarza@gmail.com>
2024-02-07 14:26:27 -0600

    Adjusted tire animation, tirestack collider, removed mesh collider, created prefab for tirestack, added way too many todos to the tire script

--- KittyHawk/Assets/TireController.cs
+++ KittyHawk/Assets/TireController.cs
@@ -5,20 +5,19 @@ using UnityEngine;
 
 public class TireController : MonoBehaviour
 {
-    private Rigidbody rb;
     private Animator anim;
     public float distanceFromKitty;
-
+    public GameObject mainCamera;
     public GameObject player;
     public float bounceForce = 10f;
     
     // Start is called before the first frame update
     void Start()
     {
-        rb = GetComponent<Rigidbody>();
         anim = GetComponent<Animator>();
     }
 
+    
     private void DetermineDistanceFromKitty()
     {
         distanceFromKitty = Vector3.Distance(player.transform.position, transform.position);
@@ -29,23 +28,35 @@ public class TireController : MonoBehaviour
         //DetermineDistanceFromKitty();
     }
 
+    // Method to set the animator back to its original state after the 
     private void ResetAnimator()
     {
         anim.Play("Normal");
     }
     
+    
     private void OnTriggerEnter(Collider c)
     {
-        
+        // When kitty collides with the tire stack, animation plays, kitty gets thrown in the air
         if (c.gameObject.CompareTag("Player"))
         {
-            Debug.Log("BOUNCING WITH KITTY");
             anim.Play("Bounce");
+
+            // Add vertical velocity to kitty
+            // TODO: Eventually this might be changed if we change how verticalVelocity works. 
+            // TODO: Should it bounce kitty only upwards or reverse her overall velocity? Can a force be added that 
+            // is the opposite force that she is walking in?
             c.GetComponent<ForceReceiver>().verticalVelocity = bounceForce;
+
+            // TODO: Kitty needs to change her animation state to jumping
             
+            // ensures the bounce finishes before the animator is reset by waiting length of the Bounce animation
             Invoke("ResetAnimator", anim.GetCurrentAnimatorStateInfo(0).length);
+            
+            // TODO: Main camera pans up? 
+            
+            //TODO: Add boing sound effect, do we need an audio engine?
         }
-        
     }
     
 }
