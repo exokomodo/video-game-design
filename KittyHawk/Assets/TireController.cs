3517c2ca56cf67506cfa3154b856a588fd183e29
Paul Garza <paulvgarza@gmail.com>
2024-02-25 16:51:54 -0600

    Level 1 Scene with Tire Stack Prefab (#56)
    * Level 1 1 scene added, basic ground texture added
    
    * Added fence to opening area
    
    * Added updated kitty hawk prefab, tire pile
    
    * Added idle animation to chicken and some chick/chicken models around the farm
    
    * Animated the gate to the garden so it can open
    
    * Added tire animation for Squash and Stretch
    
    * Tire squash and stretch
    
    * added interaction for tire bouncer
    
    * Adjusted tire animation, tirestack collider, removed mesh collider, created prefab for tirestack, added way too many todos to the tire script
    
    * Moved some files, added animator controller folder to our section
    
    * Remove idea and DS_Store
    
    * Fix animation event
    
    ---------
    
    Co-authored-by: James Orson <jamesaorson@gmail.com>

---
+++ KittyHawk/Assets/TireController.cs
@@ -0,0 +1,62 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TireController : MonoBehaviour
+{
+    private Animator anim;
+    public float distanceFromKitty;
+    public GameObject mainCamera;
+    public GameObject player;
+    public float bounceForce = 10f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    
+    private void DetermineDistanceFromKitty()
+    {
+        distanceFromKitty = Vector3.Distance(player.transform.position, transform.position);
+    }
+
+    void Update()
+    {
+        //DetermineDistanceFromKitty();
+    }
+
+    // Method to set the animator back to its original state after the 
+    private void ResetAnimator()
+    {
+        anim.Play("Normal");
+    }
+    
+    
+    private void OnTriggerEnter(Collider c)
+    {
+        // When kitty collides with the tire stack, animation plays, kitty gets thrown in the air
+        if (c.gameObject.CompareTag("Player"))
+        {
+            anim.Play("Bounce");
+
+            // Add vertical velocity to kitty
+            // TODO: Eventually this might be changed if we change how verticalVelocity works. 
+            // TODO: Should it bounce kitty only upwards or reverse her overall velocity? Can a force be added that 
+            // is the opposite force that she is walking in?
+            c.GetComponent<ForceReceiver>().verticalVelocity = bounceForce;
+
+            // TODO: Kitty needs to change her animation state to jumping
+            
+            // ensures the bounce finishes before the animator is reset by waiting length of the Bounce animation
+            Invoke("ResetAnimator", anim.GetCurrentAnimatorStateInfo(0).length);
+            
+            // TODO: Main camera pans up? 
+            
+            //TODO: Add boing sound effect, do we need an audio engine?
+        }
+    }
+    
+}
