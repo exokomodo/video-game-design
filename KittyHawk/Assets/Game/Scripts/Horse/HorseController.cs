cf54ff1d833db46f98656550433827eeff7f49c3
James Orson <jamesaorson@gmail.com>
2024-02-27 10:16:38 -0800

    Make horse move towards waypoint

--- KittyHawk/Assets/Game/Scripts/Horse/HorseController.cs
+++ KittyHawk/Assets/Game/Scripts/Horse/HorseController.cs
@@ -1,37 +1,53 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class HorseController : MonoBehaviour
 {
     #region Unity Components
+    private GameObject _waypointRoot;
     private Animator _animator;
+    private int _waypointIndex;
     #endregion
-
     public float Velocity = 0f;
     public const float VelocityScale = 5f;
 
-    public Vector3 ActualForward => -transform.right;
+    #region Private methods
+    private void FaceWaypoint()
+    {
+        var direction = Vector3.Normalize(
+            GetWaypointTransform().position - transform.position);
+        transform.forward = new Vector3(
+            direction.x,
+            0f,
+            direction.z);
+    }
+    private int GetNumberOfWaypoints() => _waypointRoot.transform.childCount;
+    private Transform GetWaypointTransform() => _waypointRoot.transform.GetChild(_waypointIndex);
+    #endregion
 
     #region Unity hooks
     private void Start()
     {
-        this._animator = GetComponent<Animator>();        
+        var waypoints = GameObject.FindGameObjectsWithTag("HorseWaypoints");
+        if (waypoints.Length > 0) {
+            _waypointRoot = waypoints[0];
+        }
+        _animator = GetComponentInChildren<Animator>();
+        _waypointIndex = 0;
     }
 
     private void FixedUpdate()
     {
-        this.transform.position += (
-            ActualForward *
+        _animator.SetFloat("Speed", Velocity);
+        transform.position += (
+            Time.fixedDeltaTime *
             Velocity *
             VelocityScale *
-            Time.fixedDeltaTime
-        );
+            transform.forward);
     }
 
     private void Update()
     {
-        this._animator.SetFloat("Speed", this.Velocity);
+        FaceWaypoint();
     }
     #endregion
 }
