877946f245a1902686fd7db9833f45a194ccf745
James Orson <jamesaorson@gmail.com>
2024-02-28 06:18:13 -0800

    horse: follows waypoints in child order

--- KittyHawk/Assets/Game/Scripts/Horse/HorseController.cs
+++ KittyHawk/Assets/Game/Scripts/Horse/HorseController.cs
@@ -1,37 +1,64 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class HorseController : MonoBehaviour
 {
     #region Unity Components
+    private GameObject _waypointRoot;
     private Animator _animator;
+    private int _waypointIndex;
     #endregion
-
     public float Velocity = 0f;
     public const float VelocityScale = 5f;
 
-    public Vector3 ActualForward => -transform.right;
+    #region Private methods
+    private void FaceWaypoint()
+    {
+        var direction = Vector3.Normalize(
+            GetWaypointTransform().position - transform.position);
+        transform.forward = new Vector3(
+            direction.x,
+            0f,
+            direction.z);
+    }
+    private int GetNumberOfWaypoints() => _waypointRoot.transform.childCount;
+    private void NextWaypoint() => _waypointIndex = (_waypointIndex + 1) % GetNumberOfWaypoints();
+    private Transform GetWaypointTransform() => _waypointRoot.transform.GetChild(_waypointIndex);
+    #endregion
 
     #region Unity hooks
     private void Start()
     {
-        this._animator = GetComponent<Animator>();        
+        var waypoints = GameObject.FindGameObjectsWithTag("HorseWaypoints");
+        if (waypoints.Length > 0)
+        {
+            _waypointRoot = waypoints[0];
+        }
+        _animator = GetComponentInChildren<Animator>();
+        _waypointIndex = 0;
     }
 
     private void FixedUpdate()
     {
-        this.transform.position += (
-            ActualForward *
+        transform.position += (
+            Time.fixedDeltaTime *
             Velocity *
             VelocityScale *
-            Time.fixedDeltaTime
-        );
+            transform.forward);
+    }
+
+    private void OnTriggerEnter(Collider c)
+    {
+        Debug.Log("Touched waypoint");
+        if (c.CompareTag("HorseWaypoint"))
+        {
+            NextWaypoint();
+        }
     }
 
     private void Update()
     {
-        this._animator.SetFloat("Speed", this.Velocity);
+        _animator.SetFloat("Speed", Velocity);
+        FaceWaypoint();
     }
     #endregion
 }
