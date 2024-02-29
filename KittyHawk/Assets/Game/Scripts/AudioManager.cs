4973582d36c15cb2083fd2a16c30de6873a3fedb
fc85057 <cf85057@outlook.com>
2024-02-29 18:55:51 -0500

    Modified audio manager controller to include general audio event

--- KittyHawk/Assets/Game/Scripts/AudioManager.cs
+++ KittyHawk/Assets/Game/Scripts/AudioManager.cs
@@ -4,25 +4,34 @@ using UnityEngine.Events;
 
 public class AudioManager : MonoBehaviour
 {
+
+    private float volume = 1f;
+
+    public AudioClip[] audioClips;
+
     public AudioClip tireStackBounceAudio;
     private UnityAction<Vector3> tireStackBounceListener;
+    private UnityAction<Vector3, string> audioEventListener;
 
     #region Unity Hooks
     private void Awake()
     {
         tireStackBounceListener = new UnityAction<Vector3>(tireStackBounceEventHandler);
+        audioEventListener = new UnityAction<Vector3, string>(audioEventHandler);
     }
 
     private void OnEnable()
     {
 
         EventManager.StartListening<TireStackBounceEvent, Vector3>(tireStackBounceListener);
+        EventManager.StartListening<AudioEvent, Vector3, string>(audioEventListener);
 
     }
 
     private void OnDisable()
     {
         EventManager.StopListening<TireStackBounceEvent, Vector3>(tireStackBounceListener);
+        EventManager.StopListening<AudioEvent, Vector3, string>(audioEventListener);
     }
     #endregion 
 
@@ -30,5 +39,16 @@ public class AudioManager : MonoBehaviour
     {
         Debug.Log(tireStackBounceAudio);
         AudioSource.PlayClipAtPoint(tireStackBounceAudio, position, 1f);
+    }
+
+    void audioEventHandler(Vector3 position, string clipName)
+    {
+        foreach (AudioClip audioClip in audioClips)
+        {
+            if (audioClip.name == clipName)
+            {
+                AudioSource.PlayClipAtPoint(audioClip, position, volume);
+            }
+        }
     }
 }
