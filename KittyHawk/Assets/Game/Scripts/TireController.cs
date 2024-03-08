bb3451b353e2fd73d6f42764930fee05818a5ee8
Paul Garza <paulvgarza@gmail.com>
2024-03-07 20:36:13 -0600

    Fixed changes requested in PR #84, updated tire prefab to have capsule collider, tire is now much less expensive. (#100)
    Co-authored-by: Geoffrey Roth <geoffroth@gmail.com>

---
+++ KittyHawk/Assets/Game/Scripts/TireController.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TireController : MonoBehaviour
+{
+    private Animator tireAnim;
+
+    public GameObject player;
+    private Rigidbody playerRb;
+    private PlayerController playerController;
+    
+    [SerializeField] private float bounceForce = 10f;
+    [SerializeField] private Vector3 approachDirection;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        tireAnim = GetComponent<Animator>();
+        playerRb = player.GetComponent<Rigidbody>();
+        playerController = player.GetComponent<PlayerController>();
+    }
+
+    // Method to set the animator back to its original state after the tire bounces.
+    // Needed to make sure the animation plays before kitty can bounce on it again.
+    private void ResetAnimator()
+    {
+        tireAnim.Play("Normal");
+    }
+
+    private void OnCollisionEnter(Collision c)
+    {
+        // When kitty collides with the tire stack, animation plays, kitty gets thrown in the air
+        if (c.gameObject.CompareTag("Player") && tireAnim.GetCurrentAnimatorStateInfo(0).length > 0)
+        {
+            tireAnim.Play("Bounce");
+
+            // Calculate the direction kitty hits the tire stack from
+            approachDirection = player.transform.position - transform.position;
+            approachDirection.y = 0;
+            approachDirection = approachDirection.normalized;
+
+            // Add vertical velocity to kitty, keeps her remaining velocity, and adds force to the x and z bounce
+            // TODO: This doesn't feel natural yet
+            playerRb.velocity = new Vector3(approachDirection.x * -2f , bounceForce, approachDirection.z * -2f); 
+            
+            // Ends jump animation so falling can begin and kitty can jump
+            EventManager.TriggerEvent<AnimationStateEvent, AnimationStateEventBehavior.AnimationEventType, 
+                string>(AnimationStateEventBehavior.AnimationEventType.TIME, AnimationStateEvent.JUMP_COMPLETE);
+            
+            playerController.SwitchToFallState();
+            
+            // ensures the bounce finishes before the animator is reset by waiting length of the Bounce animation
+            Invoke("ResetAnimator", tireAnim.GetCurrentAnimatorStateInfo(0).length);
+            
+            EventManager.TriggerEvent<TireStackBounceEvent, Vector3>(transform.position);
+        }
+    }
+    
+}
