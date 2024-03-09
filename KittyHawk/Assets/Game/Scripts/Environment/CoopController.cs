0d3ae01d252a0fac9d8e01d882a79e6d25334469
Paul Garza <paulvgarza@gmail.com>
2024-03-09 09:34:40 -0600

    Added in 3 chickens, created navmesh, moved coops, updated coopgroup with controller to handle individual coops, updated coopController.cs, set chicksToWin to 3 for coopGroupController. This currently has to be entered in a public field. (#111)

--- KittyHawk/Assets/Game/Scripts/Environment/CoopController.cs
+++ KittyHawk/Assets/Game/Scripts/Environment/CoopController.cs
@@ -5,46 +5,25 @@ using UnityEngine;
 
 public class CoopController : MonoBehaviour
 {
-
-    private int chickenCounter = 0;
-    public GameObject[] chicks;
-    public int chicksToWin;
-    public bool winnerWinnerChickenDinner;
+    public GameObject parentCoopGroup;
+    private CoopGroupController parentCoopGroupController;
     
     // Start is called before the first frame update
     void Start()
     {
+        parentCoopGroupController = parentCoopGroup.GetComponent<CoopGroupController>();
         //TODO: Implement array and collect animators to play animation of walking in coop while 
         //coop door swings open
-        //chicksToWin = chicks.Length;
-        winnerWinnerChickenDinner = false;
     }
-
-    public void checkWinStatus()
-    {
-        if (chickenCounter >= chicksToWin)
-        {
-            winnerWinnerChickenDinner = true;
-        }
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if (!winnerWinnerChickenDinner) return;
-        {
-            Debug.Log("YOU WIN!");
-            //TODO: Insert UI commands here?
-        }
-    }
+    
 
     private void OnCollisionEnter(Collision other)
     {
         // On collision with chicken increases the chicken counter and checks if it was enough to win.
         if (other.gameObject.CompareTag("Chick"))
         {
-            chickenCounter++;
-            checkWinStatus();
+            parentCoopGroupController.addChicken();
+            parentCoopGroupController.checkForWin();
         }
     }
 }
