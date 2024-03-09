e4231b0b6ab437c611a82fa50e9d093d55169e4d
James Orson <jamesaorson@gmail.com>
2024-03-09 13:53:25 -0800

    Add TagSelector attribute

---
+++ KittyHawk/Assets/Game/Scripts/Attributes/TagSelectorAttribute.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+namespace KittyHawk.Attributes
+{ 
+    public class TagSelectorAttribute : PropertyAttribute
+    {
+        public bool UseDefaultTagFieldDrawer = false;
+    }
+}
