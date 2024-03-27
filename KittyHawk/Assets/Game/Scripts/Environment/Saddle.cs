6e7a3484becd92edd8a74c8828c7c9a6aefdc460
James Orson <jamesaorson@gmail.com>
2024-03-27 14:48:39 -0700

    Use InputReader for saddle. Add left and right carrot

--- KittyHawk/Assets/Game/Scripts/Environment/Saddle.cs
+++ KittyHawk/Assets/Game/Scripts/Environment/Saddle.cs
@@ -1,5 +1,4 @@
 using System.Linq;
-using UnityEditor;
 using UnityEngine;
 
 /// <summary>
@@ -12,10 +11,15 @@ public class Saddle : MonoBehaviour
     public Vector3 SaddleHeight = Vector3.zero;
     [KittyHawk.Attributes.TagSelector]
     public string RiderTag = "Player";
+    public GameObject CarrotForward;
+    public GameObject CarrotLeft;
+    public GameObject CarrotRight;
     #endregion
 
     #region Protected properties
+    protected InputReader _input;
     protected GameObject _rider = null;
+    protected WaypointAI _waypointAI;
     #endregion
 
     #region Unity hooks
@@ -26,11 +30,23 @@ public class Saddle : MonoBehaviour
             Debug.Log("Saddle enter");
             Debug.Log("Rider entered");
             _rider = GameObject.FindGameObjectsWithTag("Player")
-                .Where(x => x.GetComponent<PlayerController>()).FirstOrDefault();
+                .FirstOrDefault(x => x.GetComponent<PlayerController>());
             EventManager.TriggerEvent<RiderEnterEvent, Saddle, GameObject>(
                 this,
                 _rider);
+            _waypointAI.SetCarrot(CarrotForward);
+            _input.JumpEvent += Dismount;
         }
+    }
+
+    private void TurnLeft()
+    {
+        _waypointAI.SetCarrot(CarrotLeft);
+    }
+
+    private void TurnRight()
+    {
+        _waypointAI.SetCarrot(CarrotRight);
     }
 
     private void OnDrawGizmos()
@@ -43,24 +59,27 @@ public class Saddle : MonoBehaviour
     {
         if (_rider != null)
         {
-            if (InputMap.IsJumping)
-            {
-                EventManager.TriggerEvent<RiderExitEvent, Saddle, GameObject>(
-                    this,
-                    _rider);
-                _rider = null;
-            }
-            else
-            {
-                _rider.transform.position = transform.position + SaddleHeight;
-            }
+            _rider.transform.position = transform.position + SaddleHeight;
         }
+    }
+
+    private void Dismount()
+    {
+        EventManager.TriggerEvent<RiderExitEvent, Saddle, GameObject>(
+            this,
+            _rider);
+        _rider = null;
+        _waypointAI.SetCarrot(null);
+        _input.JumpEvent -= Dismount;
     }
 
     private void Start()
     {
+        _input = GetComponent<InputReader>();
+        _waypointAI = GetComponent<WaypointAI>();
         Debug.Assert(GetComponent<Rigidbody>() != null, "Saddle must have a rigidbody");
         Debug.Assert(GetComponents<Collider>().Length > 0, "Saddle must at least one collider");
+        Debug.Assert(_waypointAI != null, "Saddle must have a WaypointAI component");
     }
     #endregion
 }
