587311a2fb6ddd431034ba0feb295f60995b9674
Benjamin Lee <benjaminlee096@gmail.com>
2024-04-03 18:12:16 -0500

    a commit where everything seemingly works

--- KittyHawk/Assets/Game/Scripts/CowFollow.cs
+++ KittyHawk/Assets/Game/Scripts/CowFollow.cs
@@ -1,4 +1,7 @@
+using TMPro;
+using UnityEditor.SearchService;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 
     // Author Ben Lee
@@ -15,15 +18,26 @@ public class CowFollow : MonoBehaviour
     public Canvas canvas;
     public Animator animator;
     public GameObject player;
-
     public GameObject ball;
-
     public GameObject spawn;
-
+    public Canvas inventoryCanvas;
     private int score = 0;
-
     private float timeLeft = 60.0f;
 
+    private Vector3 heightModifier;
+    private Vector3 leftRightModifier;
+
+    public GameObject timeDisplay;
+    public GameObject scoreDisplay;
+ 
+
+    private void Start() {
+           // hide inventory canvas
+           inventoryCanvas.enabled = false;
+           Vector3 heightModifier = new Vector3(0, 10, 0);
+           Vector3 leftRightModifier = new Vector3(Random.Range(-10, 10), 0, 0);
+
+    }
     private void LaunchBall() {
         // Launch a new ball at random force towards object (transform of cow)
 
@@ -32,29 +46,18 @@ public class CowFollow : MonoBehaviour
         Rigidbody ballRb = ballObject.GetComponent<Rigidbody>();
 
         // Calculate the direction of the ball
-
-        Vector3 heightModifier = new Vector3(0, 10, 0);
-        Vector3 leftRightModifier = new Vector3(Random.Range(-10, 10), 0, 0);
         Vector3 direction = transform.position - ballObject.transform.position;
         direction += heightModifier + leftRightModifier;
- 
-
         // Calculate the force
         float force = Random.Range(4, 6);
-
         // Launch the ball
         ballRb.AddForce(direction.normalized * force, ForceMode.Impulse);
         ballRb.useGravity = true;
-        
-
-
-        
-
-      
     }
 
-    private void UpdateScore() {
-        // Update the score
+    public void UpdateScore() {
+        score++;
+        scoreDisplay.GetComponent<TextMeshProUGUI>().text = score.ToString();
     }
 
     private void OnTriggerEnter(Collider other) {
@@ -63,10 +66,9 @@ public class CowFollow : MonoBehaviour
             animator.SetBool("jumping", true);
             if (!alreadyTalked) {
                 EventManager.TriggerEvent<DialogueOpenEvent, Vector3, string>(transform.position, "CowDialogue");
-                alreadyTalked = true;
+                EventManager.StartListening<DialogueCloseEvent, string>(OnDialogueFinished);
             } else {
-                int randomInt = Random.Range(1, 4);
-                EventManager.TriggerEvent<DialogueOpenEvent, Vector3, string>(transform.position, "CowFollowupDialogue" + randomInt);
+                EventManager.TriggerEvent<DialogueOpenEvent, Vector3, string>(transform.position, "CowFollowupDialogue");
             }
         }
     }
@@ -77,27 +79,35 @@ public class CowFollow : MonoBehaviour
         }
     }
 
+    private void OnDialogueFinished(string dialogueName) {
+        // enable canvas
+        inventoryCanvas.enabled = true;
+        alreadyTalked = true;
+        EventManager.StopListening<DialogueCloseEvent, string>(OnDialogueFinished);
+    }
+
     private void FixedUpdate() {
 
-
-        // randomly determine if a ball should be launched
-        if (Random.Range(0, 200) == 1) {
-            LaunchBall();
+        if (alreadyTalked) {
+            timeLeft -= Time.deltaTime;
+            timeDisplay.GetComponent<TextMeshProUGUI>().text = timeLeft.ToString("F2")+" sec";
+            if (timeLeft < 0) {
+                // end the game
+                alreadyTalked = false;
+                if (score >= 5) {
+                //    success
+                }
+                else {
+                // restart scene
+                SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+                }
+                
+            }
+            // randomly determine if a ball should be launched
+            if (Random.Range(0, 200) == 1) {
+                LaunchBall();
+            }
         }
-
-
-
-
-        // handle score display
-        
-        // handle calls to launch ball
-
-        // handle calls to update score
-
-        // handle timer
-
-        // listen for ball collisions with pond surface
-
     }
 
 }
