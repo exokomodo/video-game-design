398bd64bd56449f7edf0e3c9e432fef72cd597e5
Geoffrey Roth <geoffroth@gmail.com>
2024-04-17 15:58:14 -0400

    Tidy up, add comments

--- KittyHawk/Assets/Game/Scripts/Levels/ChickLevelController.cs
+++ KittyHawk/Assets/Game/Scripts/Levels/ChickLevelController.cs
@@ -15,20 +15,23 @@ public class ChickLevelController : MonoBehaviour {
         public static string CHICKS = "ChickObjective";
     }
 
-    private Dictionary<string, List<string>> objectiveMap = new Dictionary<string,List<string>>{
-        {Objectives.COW, new List<string>{Objectives.KEY, Objectives.CHICKEN}},
-        {Objectives.KEY, new List<string>{Objectives.CHICKEN, Objectives.COOP}},
-        {Objectives.CHICKEN, new List<string>{Objectives.KEY, Objectives.COOP}}
+    // objectiveMap sequences objectives
+    // completing the objective denoted by the key displays markers for the
+    // corresponding list of new objectives
+    private Dictionary<string, List<string>> objectiveMap = new Dictionary<string, List<string>>{
+        {Objectives.COW,        new List<string>{Objectives.KEY, Objectives.CHICKEN}},
+        {Objectives.KEY,        new List<string>{Objectives.CHICKEN, Objectives.COOP}},
+        {Objectives.CHICKEN,    new List<string>{Objectives.KEY, Objectives.COOP}}
     };
+
     void Start() {
-        // Debug.Log("Objective > START");
         EventManager.StartListening<ObjectiveChangeEvent, string, ObjectiveStatus>(OnObjectiveChange);
         EventManager.StartListening<DialogueOpenEvent, Vector3, string>(OnDialogOpen);
-        EventManager.StartListening<DialogueCloseEvent, string>(OnDialogClose);
-        Invoke("Init", 0.1f);
+        Invoke("Init", 0.1f); // Delay required to avoid race condition with LevelManager
     }
 
     private void Init() {
+        // Initial objective designated here
         UpdateObjective(Objectives.COW, ObjectiveStatus.InProgress);
     }
 
@@ -36,9 +39,9 @@ public class ChickLevelController : MonoBehaviour {
         if (status == ObjectiveStatus.Completed) {
             if (objectiveMap.ContainsKey(name)) {
                 foreach (string objName in objectiveMap[name]) {
-                    Debug.Log($"objective > objName > {objName}");
+                    // Debug.Log($"objective > objName > {objName}");
                     Objective pending = levelManager.objectivesDic[objName];
-                    Debug.Log($"objective > pending name > {pending.ObjectiveName}, status: {pending.Status}");
+                    // Debug.Log($"objective > pending name > {pending.ObjectiveName}, status: {pending.Status}");
                     if (pending.Status != ObjectiveStatus.Completed)
                         UpdateObjective(objName, ObjectiveStatus.InProgress);
                 }
@@ -57,16 +60,12 @@ public class ChickLevelController : MonoBehaviour {
         }
     }
 
-    private void OnDialogClose(string dialogueName)
-    {
-
-    }
-
     private void UpdateObjective(string name, ObjectiveStatus status) {
         EventManager.TriggerEvent<ObjectiveChangeEvent, string, ObjectiveStatus>(name, status);
     }
 
     private void OnDestroy() {
         EventManager.StopListening<ObjectiveChangeEvent, string, ObjectiveStatus>(OnObjectiveChange);
+        EventManager.StopListening<DialogueOpenEvent, Vector3, string>(OnDialogOpen);
     }
 }
