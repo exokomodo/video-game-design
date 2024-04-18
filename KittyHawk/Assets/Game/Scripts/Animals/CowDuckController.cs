fe27a89e85ff99f383d09b257d3371a96fbbbbf6
Geoffrey Roth <geoffroth@gmail.com>
2024-04-18 14:25:34 -0400

    Allow cow to jump while talking, prevent kitty from jumping at dialogue close, allow player to talk to cow more than once

--- KittyHawk/Assets/Game/Scripts/Animals/CowDuckController.cs
+++ KittyHawk/Assets/Game/Scripts/Animals/CowDuckController.cs
@@ -4,30 +4,46 @@ public class CowDuckController : MonoBehaviour
 {
 
     private Animator anim;
-    private bool alreadyTalked;
+    private float talkCooldown = 5f;
+    private float timer;
+    private bool isTalking = false;
 
     private void Start()
     {
         anim = GetComponent<Animator>();
-        alreadyTalked = false;
+        timer = talkCooldown;
+        EventManager.StartListening<DialogueCloseEvent, string>(OnDialogClose);
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.tag == "Player" && !alreadyTalked)
+        // Allow Kitty to talk to cow again, but only after a 5 second cool down
+        // in order to avoid accidental re-triggering of dialogue
+        if (other.CompareTag("Player") && timer > talkCooldown)
         {
             anim.SetBool("jumping", true);
+            anim.Play("Jumping");
+            timer = 0;
+            isTalking = true;
             EventManager.TriggerEvent<DialogueOpenEvent, Vector3, string>(transform.position, "CowDuckDialogue");
-            alreadyTalked = true;
         }
     }
 
-    private void OnTriggerExit(Collider other)
-    {
-        if (other.tag == "Player")
-        {
+    private void Update() {
+        if (!isTalking) timer += Time.deltaTime;
+    }
+
+    private void OnDialogClose(string name) {
+        // OnTriggerExit was being immediately called when the dialogue opened
+        // This caused the cow to stop jumping (I think we wanted the cow to keep jumping during the dialogue).
+        if (name == "CowDuckDialogue") {
+            isTalking = false;
             anim.SetBool("jumping", false);
         }
+    }
+
+    private void OnDestroy() {
+        EventManager.StopListening<DialogueCloseEvent, string>(OnDialogClose);
     }
 
 }
