c0dba7b695046c520d900096aeb3575bf658800a
Geoffrey Roth <geoffroth@gmail.com>
2024-04-18 16:39:01 -0400

    Mon and Tues cows extend same base class

---
+++ KittyHawk/Assets/Game/Scripts/Animals/Cow/CowMondayController.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class CowMondayController : CowBaseController
+{
+    protected  override void Start()
+    {
+        base.Start();
+        dialogueName = "CowDialoguePaul";
+    }
+}
--- KittyHawk/Assets/Game/Scripts/Animals/CowDuckController.cs
+++ KittyHawk/Assets/Game/Scripts/Animals/CowDuckController.cs
@@ -1,49 +1,10 @@
 using UnityEngine;
 
-public class CowDuckController : MonoBehaviour
+public class CowDuckController : CowBaseController
 {
-
-    private Animator anim;
-    private float talkCooldown = 5f;
-    private float timer;
-    private bool isTalking = false;
-
-    private void Start()
+    protected override void Start()
     {
-        anim = GetComponent<Animator>();
-        timer = talkCooldown;
-        EventManager.StartListening<DialogueCloseEvent, string>(OnDialogClose);
+        base.Start();
+        dialogueName = "CowDuckDialogue";
     }
-
-    private void OnTriggerEnter(Collider other)
-    {
-        // Allow Kitty to talk to cow again, but only after a 5 second cool down
-        // in order to avoid accidental re-triggering of dialogue
-        if (other.CompareTag("Player") && timer > talkCooldown)
-        {
-            anim.SetBool("jumping", true);
-            anim.Play("Jumping");
-            timer = 0;
-            isTalking = true;
-            EventManager.TriggerEvent<DialogueOpenEvent, Vector3, string>(transform.position, "CowDuckDialogue");
-        }
-    }
-
-    private void Update() {
-        if (!isTalking) timer += Time.deltaTime;
-    }
-
-    private void OnDialogClose(string name) {
-        // OnTriggerExit was being immediately called when the dialogue opened
-        // This caused the cow to stop jumping (I think we wanted the cow to keep jumping during the dialogue).
-        if (name == "CowDuckDialogue") {
-            isTalking = false;
-            anim.SetBool("jumping", false);
-        }
-    }
-
-    private void OnDestroy() {
-        EventManager.StopListening<DialogueCloseEvent, string>(OnDialogClose);
-    }
-
 }
