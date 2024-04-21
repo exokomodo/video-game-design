cf6e4dd39585a4a3b14794fe204b5c47c41996fd
fc85057 <cf85057@outlook.com>
2024-04-20 22:23:50 -0400

    Add the freaking header

--- KittyHawk/Assets/Game/Scripts/EndManager.cs
+++ KittyHawk/Assets/Game/Scripts/EndManager.cs
@@ -1,9 +1,11 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEngine.UI;
 
+/// <summary>
+/// Class controlling the game's ending cutscene
+/// Author: Calvin Ferst
+/// </summary>
 public class EndManager : MonoBehaviour
 {
     [SerializeField]
